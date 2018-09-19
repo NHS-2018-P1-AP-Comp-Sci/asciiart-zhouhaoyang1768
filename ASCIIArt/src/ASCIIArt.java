df421d15d73f3f4a93d361822822c61ecad43c96
s-zhouha <s-zhouha@53S50023195.it.bsd405.org>
2018-09-19 08:47:03 -0700

    Re-Turning in project

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -14,8 +14,226 @@
  *
  */
 
+
 public class ASCIIArt {
-	public static void main (String [] args) {
-		System.out.println("THIS IS MY ASCII ART!!!!");
+	public static void main(String []arg) {
+		escapeP();
+		topP();
+		bodyP();
+		bootP();
+		fireP();
 	}
+		
+	public static void pin1() {
+		System.out.println("\t\t\t\t     ||");
+	}
+	
+	public static void pin2() {
+		System.out.println("\t\t\t\t    |  |");
+	}
+	
+	public static void body1() {
+		System.out.println("\t\t\t      |              |");
+	}
+	
+	public static void body2() {
+		System.out.println("\t\t\t       |            |");
+	}
+	
+	public static void body3() {
+		System.out.println("\t\t\t       | ||     |   |");
+		System.out.println("\t\t\t       | ||      |  |");
+	}
+	
+	public static void body4() {
+		System.out.println("\t\t\t       | ||         |");
+	}
+	
+	public static void body5() {
+		System.out.println("\t\t\t       | ||  __     |");
+//		System.out.println("\t\t\t       | || |  |    |");
+		System.out.println("\t\t\t       | || |__|    |");
+	}
+	
+	public static void body6() {
+		System.out.println("\t\t\t       |     __     |");
+//		System.out.println("\t\t\t       |    |  |    |");
+		System.out.println("\t\t\t       |    |__|    |");
+	}
+	
+	public static void boot() {
+		System.out.println("\t\t\t | |     |        |     | |");
+		System.out.println("\t\t\t |  |    |        |    |  |");
+	}
+	
+	public static void escapeP() {
+		System.out.println("\t\t\t\t     /\\");
+		pin1();
+		pin1();
+		pin1();
+		pin1();
+		System.out.println("\t\t\t\t     /\\");
+		System.out.println("\t\t\t\t    /  \\");
+		System.out.println("\t\t\t\t    \\/\\/");
+		pin1();
+		pin1();
+		pin1();
+		pin1();
+		System.out.println("\t\t\t\t     /\\");
+		System.out.println("\t\t\t\t    /  \\");
+		System.out.println("\t\t\t\t  _/    \\_");
+		System.out.println("\t\t\t\t /  _/\\_  \\");
+		System.out.println("\t\t\t\t \\_/|  |\\_/");
+		pin2();
+		pin2();
+		pin2();
+		pin2();
+		pin2();
+	}
+	
+	public static void topP() {
+
+		System.out.println("\t\t\t\t    |__|");
+		System.out.println("\t\t\t\t    /  \\");
+		System.out.println("\t\t\t\t   /    \\");
+		System.out.println("\t\t\t\t  /      \\");
+		System.out.println("\t\t\t\t /        \\");
+		System.out.println("\t\t\t\t/          \\");
+		System.out.println("\t\t\t       /            \\");
+		System.out.println("\t\t\t      /______________\\");
+		System.out.println("\t\t\t      \\______________/");
+		body1();
+		body1();
+		body1();
+		body1();
+		System.out.println("\t\t\t     /|   ________   |\\");
+		System.out.println("\t\t\t     ||  |hello   |  ||");
+		System.out.println("\t\t\t     ||  |   world|  ||");
+		System.out.println("\t\t\t     \\|  |________|  |/");
+		body1();
+		body1();
+		body1();
+		body1();
+		System.out.println("\t\t\t      /______________\\");
+		System.out.println("\t\t\t      \\              /");
+		System.out.println("\t\t\t       \\____________/");
+	}
+	
+	public static void bodyP() {
+		body2();
+		body2();
+		System.out.println("\t\t\t       |____________|");
+		body3();
+		body3();
+		body3();
+		body3();
+		System.out.println("\t\t\t       | ||     |   |");
+		System.out.println("\t\t\t       |_||______|__|");
+		System.out.println("\t\t\t       |_||_________|");
+		body4();
+		body4();
+		body4();
+		System.out.println("\t\t\t       | ||  /\\     |");
+		System.out.println("\t\t\t       | || /__\\    |");
+		body4();
+		body4();
+		body5();
+		body4();
+		body2();
+		body6();
+		body2();
+		body2();
+		body6();
+		body4();
+		body4();
+		body5();
+		body4();
+		body4();
+		body4();
+		System.out.println("\t\t\t       |_||_________|");
+		System.out.println("\t\t\t       |_||_________|");
+		body4();
+		body4();
+		System.out.println("\t\t\t       | ||  C      |");
+		body4();
+		System.out.println("\t\t\t       | ||  Z      |");
+		body4();
+		System.out.println("\t\t\t       | ||  |      |");
+		body4();
+		System.out.println("\t\t\t       | ||  2      |");
+		body4();
+		System.out.println("\t\t\t       | ||  F      |");
+		body4();
+		body4();
+		
+	}
+	
+	public static void bootP() {
+		System.out.println("\t\t\t     _ |            | _");
+		System.out.println("\t\t\t    / \\|            |/ \\");
+		System.out.println("\t\t\t   /   \\            /   \\");
+		System.out.println("\t\t\t  /     \\          /     \\");
+		System.out.println("\t\t\t /_______\\        /_______\\");
+		System.out.println("\t\t\t \\_______/        \\_______/");
+		boot();
+		boot();
+		boot();
+		boot();
+		boot();
+		boot();
+		System.out.println("\t\t\t | |     |        |     | |");
+		System.out.println("\t\t\t/|  |    |        |    |  |\\");
+		System.out.println("\t\t       / | |     |        |     | | \\");
+		System.out.println("\t\t      /  |  |    |        |    |  |  \\");
+		System.out.println("\t\t      |  | |     |        |     | |  |");
+		System.out.println("\t\t      /__|  |    |        |    |  |__\\");
+		System.out.println("\t\t      \\  |_|_____|        |_____|_|  /");
+		System.out.println("\t\t       \\_/       \\        /       \\_/");
+		System.out.println("\t\t\t \\_______/        \\_______/");
+		System.out.println("\t\t\t   /___\\            /___\\");
+		System.out.println("\t\t\t       \\____________/");
+		System.out.println("\t\t\t        /___\\  /___\\");
+	}
+	
+	public static void fireP() {
+		System.out.println("\t\t\t    /\\                /\\");
+		System.out.println("\t\t\t   //\\\\              //\\\\");
+		System.out.println("\t\t\t   //\\\\  / \\    / \\  //\\\\");
+		System.out.println("\t\t\t  //  \\\\// \\\\  // \\\\//  \\\\");
+		System.out.println("\t\t\t  //  \\\\// \\\\  // \\\\//  \\\\");
+		System.out.println("\t\t\t  //  \\\\/   \\\\//   \\//  \\\\");
+		System.out.println("\t\t\t //    \\\\   \\\\//   //    \\\\");
+		System.out.println("\t\t\t //    \\\\    \\/    //    \\\\");
+		System.out.println("\t\t\t //    \\\\    \\/    //    \\\\");
+		System.out.println("\t\t\t //    \\\\          //    \\\\");
+		System.out.println("\t\t\t//      \\\\        //      \\\\");
+		System.out.println("\t\t\t//      \\\\   /\\   //      \\\\");
+		System.out.println("\t\t\t\\\\      //   /\\   \\\\      //");
+		System.out.println("\t\t\t\\\\      //  //\\\\  \\\\      //");
+		System.out.println("\t\t\t \\\\    //   //\\\\   \\\\    // ");
+		System.out.println("\t\t\t \\\\    //   \\\\//   \\\\    //");
+		System.out.println("\t\t\t \\\\    //   \\\\//   \\\\    //");
+		System.out.println("\t\t\t \\\\    //    \\/    \\\\    //");
+		System.out.println("\t\t\t  \\\\  //     \\/     \\\\  //");
+		System.out.println("\t\t\t  \\\\  //\\          /\\\\  //");
+		System.out.println("\t\t\t  \\\\  //\\\\        //\\\\  //");
+		System.out.println("\t\t\t   \\\\// \\\\        // \\\\//");
+		System.out.println("\t\t\t   \\\\//  \\\\      //  \\\\//");
+		System.out.println("\t\t\t   \\\\//  \\\\      //  \\\\//");
+		System.out.println("\t\t\t    \\/    \\\\    //    \\/");
+		System.out.println("\t\t\t    \\/    \\\\    //    \\/");
+		System.out.println("\t\t\t    \\/     \\\\  //     \\/");
+		System.out.println("\t\t\t    \\/     \\\\  //     \\/");
+		System.out.println("\t\t\t    \\/     \\\\  //     \\/");
+		System.out.println("\t\t\t            \\\\//");
+		System.out.println("\t\t\t            \\\\//");
+		System.out.println("\t\t\t            \\\\//");
+		System.out.println("\t\t\t\t     \\/");
+		System.out.println("\t\t\t\t     \\/");
+		System.out.println("\t\t\t\t     \\/");
+		System.out.println("\t\t\t\t     \\/");
+		System.out.println("\t\t\t\t     \\/");
+		System.out.println("\t\t\t\t     \\/");
+	}
+		
 }
